413325e95b2750c3ebc2f23abbe95624f1c24469
SANKET SARKAR <47353707+sscodes@users.noreply.github.com>
2021-04-17 21:58:24 +0530

    Update Signup.jsx

--- client/src/containers/SignUp/Signup.jsx
+++ client/src/containers/SignUp/Signup.jsx
@@ -32,7 +32,7 @@ const Signup = () => {
       return false;
     }
     if (!password.includes('1') && !password.includes('2') && !password.includes('3') && !password.includes('4') && !password.includes('5') && !password.includes('6') && !password.includes('7') && !password.includes('8') && !password.includes('9') && !password.includes('0') && !password.includes('A') && !password.includes('B') && !password.includes('C') && !password.includes('D') && !password.includes('E') && !password.includes('F') && !password.includes('G') && !password.includes('H') && !password.includes('I') && !password.includes('J') && !password.includes('K') && !password.includes('L') && !password.includes('M') && !password.includes('N') && !password.includes('O') && !password.includes('P') && !password.includes('Q') && !password.includes('R') && !password.includes('S') && !password.includes('T') && !password.includes('U') && !password.includes('V') && !password.includes('W') && !password.includes('X') && !password.includes('Y') && !password.includes('Z')) {
-      setError("Password must contain at least one number and one capital number...");
+      setError("Password must contain at least one number and one capital letter...");
       return false;
     }
     setError('')
